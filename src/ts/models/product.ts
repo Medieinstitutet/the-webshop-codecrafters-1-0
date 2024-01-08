5b821e070f268f72385263307ff6ab5b0d5b93e9
Adamwadin <Adamwadin@live.se>
2024-01-08 10:35:19 +0100

    checkoutpage

--- src/ts/models/product.ts
+++ src/ts/models/product.ts
@@ -1,8 +1,9 @@
-export interface Product{
-    _id: string,
-    titel: string,
-    image:string,
-    price: number,
-    description:[]
-
-    }
+export interface Product {
+  _id: string;
+  titel: string;
+  image: string;
+  price: number;
+  id: string;
+  uniqueId: string;
+  amount: number;
+}
