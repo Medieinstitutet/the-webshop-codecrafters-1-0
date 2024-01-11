595b323902918201975e357a0c60c1063c1012d3
Adam_Wadin <adamwadin@live.se>
2024-01-11 11:48:25 +0100

    Revert "styling"
    This reverts commit 6b6d0bb359c44e00911252d3cba8a2cbbed475d9.

--- src/ts/checkoutPage.ts
+++ src/ts/checkoutPage.ts
@@ -40,7 +40,6 @@ export const renderProductsInDOM = (products: Product[]): void => {
 
     groupedProducts.forEach((product) => {
       const productElement = document.createElement("ul");
-      
 
       productElement.innerHTML = `
       
